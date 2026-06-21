1d639714930c29ea89ba18a0943a4f90d9ffb293
Atul Varma <varmaa@gmail.com>
2026-06-21 06:49:57 -0400

    End of step 2.

--- src/didact.js
+++ src/didact.js
@@ -19,12 +19,36 @@
 //   Step VII  — Function Components
 //   Step VIII — Hooks (useState)
 
+const TEXT_ELEMENT = "TEXT_ELEMENT";
+
 function createElement(type, props, ...children) {
-  throw new Error("Didact.createElement: not implemented yet — Step I");
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child))
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: TEXT_ELEMENT,
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
 }
 
 function render(element, container) {
-  throw new Error("Didact.render: not implemented yet — Step II");
+  const dom = element.type === TEXT_ELEMENT ? document.createTextNode("") : document.createElement(element.type);
+  const isProperty = key => key !== "children";
+  Object.keys(element.props).filter(isProperty).forEach(name => {
+    dom[name] = element.props[name];
+  });
+  element.props.children.forEach(child => render(child, dom));
+  container.appendChild(dom);
 }
 
 const Fragment = "FRAGMENT";
